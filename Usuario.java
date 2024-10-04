dbe985abe7e694e0a0d31a5e173a6b907d8d5e3d
Gustavo Trevisol <gustavotrevisol@MacBook-Air-de-Gustavo-4.local>
2024-10-03 21:04:57 -0300

    Construtor da classe usuario

--- Usuario.java
+++ Usuario.java
@@ -1,3 +1,11 @@
 public class Usuario {
+    private int identificador;
+    private String nome;
+    private String tipo;
     
+    public Usuario(int identificador, String nome, String tipo){
+        this.identificador = identificador;
+        
+    }
+
 }
