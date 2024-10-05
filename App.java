21b6499a9993623ef8bb3cd6ae4c594dd3dc7c76
Flávia Carvalho Tavaniello <flatavaniello@gmail.com>
2024-10-05 16:29:17 -0300

    classe departamento + dados iniciais

--- App.java
+++ App.java
@@ -1,6 +1,5 @@
-import java.time.LocalDate;
-import java.util.Scanner;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class App {
     Scanner entrada = new Scanner (System.in);
@@ -12,26 +11,14 @@ public class App {
     }
 
     public void executar() {
-        Usuario u1 = new Usuario(1101, "Flavia Tavaniello");
-        Usuario u2 = new Usuario(1102, "Gustavo Trevisol");
-        Usuario u3 = new Usuario(1103, "Helena Pacheco");
-        Usuario u4 = new Usuario(1104, "Rafaelo Mello");
-        Usuario u5 = new Usuario(1105, "Giancarlo Silva");
-        Usuario u6 = new Usuario(1106, "Pedro Reginato");
-        Usuario u7 = new Usuario(1107, "Arthur Plenz");
-        Usuario u8 = new Usuario(1108, "Pedro Favieiro");
-        Usuario u9 = new Usuario(1109, "Carlos Ornél");
-        Usuario u10 = new Usuario(1110, "Otavio Parraga");
-        Usuario u11 = new Usuario(1111, "Marcelo Yamaguti");
-        Usuario u12 = new Usuario(1112, "Brasil");
-        Usuario u13 = new Usuario(1113, "Bella");
-        Usuario u14 = new Usuario(1114, "Diaz com z");
-        Usuario u15 = new Usuario(1115, "Dias Justin");
-        Usuario u16 = new Usuario(3301, "Rogério");
+        dadosInicias();
         
+        //menu inicial
+        int cod = 0;
         while (true) {
             System.out.println("Bem-vindo ao sistema de pedidos de aquisição.");
-            System.out.println("Escolha o tipo de usuário:");
+            System.out.print("Insira o seu código identificador: ");
+            cod = entrada.nextInt();
             System.out.println("1 - Funcionário");
             System.out.println("2 - Administrador");
             System.out.println("0 - Sair");
@@ -67,6 +54,65 @@ public class App {
         
         // for (Pedido pe : p) {
         //     System.out.println(pe.toString());
-        // }
+        }
+
+        public void dadosInicias() {
+        ArrayList <Usuario> usuariosDTI = new ArrayList<>();
+        ArrayList <Usuario> usuariosDRH = new ArrayList<>();
+        ArrayList <Usuario> usuariosDMarketing = new ArrayList<>();
+        ArrayList <Usuario> usuariosDFinanceiro = new ArrayList<>();
+        ArrayList <Usuario> usuariosDAdm = new ArrayList<>();
+
+        ArrayList<Departamento> departamentos = new ArrayList<>();
+        
+        Usuario u1 = new Usuario(1101, "Flavia");
+        Usuario u2 = new Usuario(1102, "Gustavo");
+        Usuario u3 = new Usuario(1103, "Helena");
+        usuariosDTI.add(u1);
+        usuariosDTI.add(u2);
+        usuariosDTI.add(u3);
+        Departamento ti = new Departamento("TI", usuariosDTI);
+        departamentos.add(ti);
+
+        Usuario u4 = new Usuario(1104, "Rafaelo");
+        Usuario u5 = new Usuario(1105, "Giancarlo");
+        Usuario u6 = new Usuario(1106, "Pedro");
+        usuariosDRH.add(u4);
+        usuariosDRH.add(u5);
+        usuariosDRH.add(u6);
+        Departamento rh = new Departamento("RH", usuariosDRH);
+        departamentos.add(rh);
+
+        Usuario u7 = new Usuario(1107, "Arthur");
+        Usuario u8 = new Usuario(1108, "Joao");
+        Usuario u9 = new Usuario(1109, "Carlos");
+        usuariosDMarketing.add (u7);
+        usuariosDMarketing.add(u8);
+        usuariosDMarketing.add(u9);
+        Departamento marketing = new Departamento("Marketing", usuariosDMarketing);
+        departamentos.add(marketing);
+
+        Usuario u10 = new Usuario(1110, "Otavio");
+        Usuario u11 = new Usuario(1111, "Marcelo");
+        Usuario u12 = new Usuario(1112, "Henrique");
+        usuariosDFinanceiro.add(u10);
+        usuariosDFinanceiro.add(u11);
+        usuariosDFinanceiro.add(u12);
+        Departamento financeiro = new Departamento("Financeiro", usuariosDFinanceiro);
+        departamentos.add(financeiro);
+
+        Usuario u13 = new Usuario(1113, "Isabella");
+        Usuario u14 = new Usuario(1114, "Miguel");
+        Usuario u15 = new Usuario(1115, "Gustavo");
+        Usuario u16 = new Usuario(3301, "Rogerio");
+        usuariosDAdm.add(u13);
+        usuariosDAdm.add(u14);
+        usuariosDAdm.add(u15);
+        usuariosDAdm.add(u16);
+        Departamento administrativo = new Departamento("Administrativo", usuariosDAdm);
+        departamentos.add(administrativo);
+
+        empresa.setDepartamentos(departamentos);
+
+        }
     }
-}
--- Departamento.java
+++ Departamento.java
@@ -1,3 +1,39 @@
+import java.util.ArrayList;
+
 public class Departamento {
+    private String nome;
+    private ArrayList<Usuario> usuario;
+
+    public Departamento (String nome, ArrayList<Usuario> usuario) {
+        this.nome = nome;
+        this.usuario = usuario;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public ArrayList<Usuario> getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(ArrayList<Usuario> usuario) {
+        this.usuario = usuario;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Departamento{");
+        sb.append("nome=").append(nome);
+        sb.append(", usuario=").append(usuario);
+        sb.append('}');
+        return sb.toString();
+    }
+
     
-}
+    }
